2417d374834070e3fa23b694f2b65c1ef29a7771
Brian Boll <bboll@kent.edu>
2022-06-21 16:20:26 -0400

    Fixed files, renamed working name to BADI

---
+++ BADI/BADI/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BADI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO: Accept and verify user input, handle i/o and input exceptions
+            Console.WriteLine("Welcome to Brian's Animal Database Interface (BADI)");
+            Console.WriteLine("Please enter a function and press the <Enter> key");
+        }
+    }
+}
---
+++ BADI/BADIlib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BADIlib
+{
+    static public class BADIlibtools
+    {
+        //STUB: libtools object for accessing different functions
+
+        //number of library modules
+        private static int toolCount = 0;
+
+
+    }
+}
